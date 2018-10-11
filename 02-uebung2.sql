cc7bcd732c7d55b396d493c5002a3f3e5d0117b1
sEITbtschw <benedict.schwind@stud.fh-rosenheim.de>
2018-10-11 21:32:24 +0200

    insert data into database

--- 02-uebung2.sql
+++ 02-uebung2.sql
@@ -5,7 +5,7 @@
 create table Studenten
 (
 	Name varchar(30),
-	Matrikel DECIMAL(4,0) primary key,
+	Matrikel decimal(4,0) primary key,
 );
 
 create table Dozenten
@@ -27,20 +27,44 @@ create table Veranstaltungen
 
 create table Student_in_Veranstaltung
 (
-    Student DECIMAL(4,0),
+    Student decimal(4,0),
     foreign key(Student) references Studenten(Matrikel),
-	Name varchar(30),
 	Veranstaltung varchar(30),
 	Semester char(4),
     foreign key(Veranstaltung, Semester) references Veranstaltungen(Name, Semester),
 	Note decimal(2,1),
-	constraint gueltige_note check(Note >= 6 and Note <=1),
+	constraint gueltige_note check(Note < 6 and Note >= 1),
 	primary key(Student, Veranstaltung, Semester)
 );
 
 /********************************************************/
 /* fill with data										*/
 /********************************************************/
+insert into Studenten(Name, Matrikel) values
+	('Klaus','1252'),
+	('Maria','4356'),
+	('Johanna','4567'),
+	('Meemet','6969');
+
+insert into Dozenten(Name, Buero, Tel) values
+	('Schmidt','A0.01','0800902'),
+	('Ferrit','A0.05','12344567'),
+	('Heftig','A0.07','10001000');
+
+insert into Veranstaltungen(Name, Semester, Raum, Dozent) values
+	('Programmieren 15','4','B0.14','Schmidt'),
+	('Bauelemente','1','R0.05','Ferrit'),
+	('Mathematik 1','2','B0.03','Heftig');
+
+insert into Student_in_Veranstaltung(Student, Veranstaltung, Semester, Note) values
+	('1252','Programmieren 15','4','3.3'),
+	('4356','Mathematik 1','2','2.3'),
+	('4567','Programmieren 15','4','1.0'),
+	('6969','Bauelemente','1','5.0');
+
+select * from Studenten;
+select * from Dozenten;
+select * from Veranstaltungen;
 select * from Student_in_Veranstaltung;
 
 /********************************************************/
